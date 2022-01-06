e31cfd4f71251ad244e67eea399ea04590158f91
ppaka <asj0216@gmail.com>
2022-01-06 10:50:02 +0900

    현재 활동변경

--- events/ready.js
+++ events/ready.js
@@ -3,5 +3,8 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setActivity("/시간표 | /수업시간", {
+			type: "LISTENING"
+		});
 	},
 };
