c52720af8508d9743310b9d3ea7f963f8d148faf
ppaka <asj0216@gmail.com>
2022-04-04 23:41:59 +0900

    Update reset-commands.js

--- reset-commands.js
+++ reset-commands.js
@@ -1,13 +1,7 @@
 const { clientId, guildId, token } = require('./config.json');
-require('dotenv').config();
-
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-
-const token = process.env.TOKEN;
-const clientId = process.env.CLIENT_ID;
-const guildId = process.env.TEST_GUILD_ID;
     
 const rest = new REST({ version: '9' }).setToken(token);
 rest.get(Routes.applicationCommands(clientId))
