65c3c434c85e8955cf4175ee780c77b729f29384
ppaka <asj0216@gmail.com>
2022-04-04 23:41:34 +0900

    Update reset-commands.js

--- reset-commands.js
+++ reset-commands.js
@@ -1,25 +1,21 @@
-const fs = require('fs');
+const { clientId, guildId, token } = require('./config.json');
+require('dotenv').config();
+
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
-const { Client, Collection, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-        commands.push(command.data.toJSON());
-}
-
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.TEST_GUILD_ID;
+    
 const rest = new REST({ version: '9' }).setToken(token);
-
-// const guild = client.guilds.cache.get("<guild id>");
-
-//client.login(token);
-// This takes ~1 hour to update
-client.application.commands.set([]);
-// This updates immediately
-// guild.commands.set([]);
+rest.get(Routes.applicationCommands(clientId))
+    .then(data => {
+        const promises = [];
+        for (const command of data) {
+            const deleteUrl = `${Routes.applicationCommands(clientId)}/${command.id}`;
+            promises.push(rest.delete(deleteUrl));
+        }
+        return Promise.all(promises);
+    });
