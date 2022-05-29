83f0e9940643bd76dc9f475a4107f01acdc55a2f
ppaka <asj0216@gmail.com>
2022-05-30 00:10:34 +0900

    수정

--- commands/timetable.js
+++ commands/timetable.js
@@ -17,7 +17,7 @@ module.exports = {
 			.addChoice('네보1', '네트워크보안과1').addChoice('해킹1', '해킹보안과1').addChoice('해킹2', '해킹보안과2').setRequired(true))
 		.addIntegerOption(optiontwo => optiontwo.setName("day").setDescription('원하는 요일의 시간표를 볼때 사용합니다').addChoice('월요일', 1)
 			.addChoice('화요일', 2).addChoice('수요일', 3).addChoice('목요일', 4).addChoice('금요일', 5).setRequired(false)),
-	execute(interaction) {
+			async execute(interaction) {
 		const argsClass = interaction.options["_hoistedOptions"].filter((object) => {
 			return object['name'] === 'class'
 		})[0];
@@ -43,32 +43,32 @@ module.exports = {
 		var reqUrl = `https://open.neis.go.kr/hub/hisTimetable?KEY=${neisKey}&Type=json&ATPT_OFCDC_SC_CODE=${APTP_OFCDC_SC_CODE}&SD_SCHUL_CODE=${SD_SCHUL_CODE}&ALL_TI_YMD=${ALL_TI_YMD}&DDDEP_NM=${encodeURI(DDDEP_NM)}&GRADE=${GRADE}&CLASS_NM=${CLASS_NM}`
 		var reqOptions = { 'method': 'GET', 'url': reqUrl }
 
-		request(reqOptions, function (error, response, body) {
+		await interaction.deferReply();
+
+		request(reqOptions, async function (error, response, body) {
 			if (error) {
 				console.error(err);
-				interaction.reply({ content: '명령을 실행하는데 오류가 발생하였습니다...', ephemeral: true });
+				await interaction.editReply({ content: '명령을 실행하는데 오류가 발생하였습니다...', ephemeral: true });
 			} else {
 				const jsonData = JSON.parse(body);
 				if (!jsonData.hasOwnProperty('hisTimetable'))
 				{
 					if(jsonData.RESULT.CODE == "INFO-200"){
 						const errorEmbed = new MessageEmbed().setColor('#FF0000').setTitle('오류').setDescription('나이스에서 데이터를 불러올 수 없습니다').setFooter({ text: 'paka#8285' });
-						interaction.reply({ embeds: [errorEmbed] });
+						await interaction.editReply({ embeds: [errorEmbed] });
 					}
 				}
 				else{
 					if (jsonData.hisTimetable[0].head[1].RESULT.CODE != "INFO-000") {
 						const errorEmbed = new MessageEmbed().setColor('#FF0000').setTitle('오류').setDescription('나이스에서 데이터를 불러올 수 없습니다').setFooter({ text: 'paka#8285' });
-						interaction.reply({ embeds: [errorEmbed] });
+						await interaction.editReply({ embeds: [errorEmbed] });
 					}
 					else {
 						if (!jsonData.hisTimetable[1].row) {
 							const errorEmbed = new MessageEmbed().setColor('#FF0000').setTitle('오류').setDescription('시간표 데이터가 존재하지 않습니다').setFooter({ text: 'paka#8285' });
-							interaction.reply({ embeds: [errorEmbed] });
+							await interaction.editReply({ embeds: [errorEmbed] });
 						}
 						else {
-							interaction.deferReply();
-
 							function getRandomColor() {
 								var letters = '0123456789ABCDEF';
 								var color = '#';
@@ -83,7 +83,7 @@ module.exports = {
 								embed.addField(`${element.PERIO}교시`, `${element.ITRT_CNTNT}`, false);
 							});
 	
-							interaction.reply({ embeds: [embed] });
+							await interaction.editReply({ embeds: [embed] });
 						}
 					}
 				}
