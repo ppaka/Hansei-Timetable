71ca22884c4690fb880bc3db7fe947b6742375fb
ppaka <asj0216@gmail.com>
2023-09-12 21:32:26 +0900

    코드 정리

--- Cogs/get_timetable_day.py
+++ Cogs/get_timetable_day.py
@@ -12,138 +12,171 @@ import requests
 import random
 
 
-NEIS_KEY = os.environ['NEIS_KEY']
-daylist = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
-
+NEIS_KEY = os.environ["NEIS_KEY"]
+day_list = ["월요일", "화요일", "수요일", "목요일", "금요일", "토요일", "일요일"]
 
 
 class get_timetable_day(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot = bot
 
-    @app_commands.command(name='시간표', description='나이스에서 시간표 데이터를 찾아드립니다!')
-    @app_commands.describe(grade='학년을 선택해주세요.')
-    @app_commands.choices(grade=[
-        app_commands.Choice(name='1학년', value=1),
-        app_commands.Choice(name='2학년', value=2),
-        app_commands.Choice(name='3학년', value=3),
-    ])
-    #@app_commands.rename(dddep_name='class')
-    @app_commands.describe(dddep_name='학과명을 선택해주세요.')
-    @app_commands.choices(dddep_name=[
-        # app_commands.Choice(name='U-센서네트워크보안과', value='센서네트워크보안과'),
-        app_commands.Choice(name='게임과', value='게임과'),
-        app_commands.Choice(name='네트워크보안과', value='네트워크보안과'),
-        # app_commands.Choice(name='로봇전자과', value='로봇전자과'),
-        app_commands.Choice(name='메타버스게임과', value='메타버스게임과'),
-        # app_commands.Choice(name='전자계산기과', value='전자계산기과'),
-        # app_commands.Choice(name='정보보안과정', value='정보보안과정'),
-        app_commands.Choice(name='클라우드보안과', value='클라우드보안과'),
-        app_commands.Choice(name='해킹보안과', value='해킹보안과'),
-        # app_commands.Choice(name='해킹보안과정', value='해킹보안과정'),
-    ])
-    @app_commands.describe(class_name='반을 입력해주세요.')
-    @app_commands.describe(weekday='원하는 요일의 시간표를 볼때 사용합니다.')
-    @app_commands.choices(weekday=[
-        app_commands.Choice(name='월요일', value=0),
-        app_commands.Choice(name='화요일', value=1),
-        app_commands.Choice(name='수요일', value=2),
-        app_commands.Choice(name='목요일', value=3),
-        app_commands.Choice(name='금요일', value=4),
-    ])
-    @app_commands.describe(date='원하는 날짜의 시간표를 볼때 사용합니다. (입력예시: 20220505)')
-    async def request_get(self, interaction: Interaction, grade: int, dddep_name: app_commands.Choice[str], class_name: int = 1, weekday: Optional[app_commands.Choice[int]] = None, date: Optional[int] = None) -> None:
-        cur_date = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    @app_commands.command(name="시간표", description="나이스에서 시간표 데이터를 찾아드립니다!")
+    @app_commands.describe(grade="학년을 선택해주세요.")
+    @app_commands.choices(
+        grade=[
+            app_commands.Choice(name="1학년", value=1),
+            app_commands.Choice(name="2학년", value=2),
+            app_commands.Choice(name="3학년", value=3),
+        ]
+    )
+    # @app_commands.rename(dddep_name='class')
+    @app_commands.describe(dddep_name="학과명을 선택해주세요.")
+    @app_commands.choices(
+        dddep_name=[
+            # app_commands.Choice(name='U-센서네트워크보안과', value='센서네트워크보안과'),
+            app_commands.Choice(name="게임과", value="게임과"),
+            app_commands.Choice(name="네트워크보안과", value="네트워크보안과"),
+            # app_commands.Choice(name='로봇전자과', value='로봇전자과'),
+            app_commands.Choice(name="메타버스게임과", value="메타버스게임과"),
+            # app_commands.Choice(name='전자계산기과', value='전자계산기과'),
+            # app_commands.Choice(name='정보보안과정', value='정보보안과정'),
+            app_commands.Choice(name="클라우드보안과", value="클라우드보안과"),
+            app_commands.Choice(name="해킹보안과", value="해킹보안과"),
+            # app_commands.Choice(name='해킹보안과정', value='해킹보안과정'),
+        ]
+    )
+    @app_commands.describe(class_name="반을 입력해주세요.")
+    @app_commands.describe(weekday="원하는 요일의 시간표를 볼때 사용합니다.")
+    @app_commands.choices(
+        weekday=[
+            app_commands.Choice(name="월요일", value=0),
+            app_commands.Choice(name="화요일", value=1),
+            app_commands.Choice(name="수요일", value=2),
+            app_commands.Choice(name="목요일", value=3),
+            app_commands.Choice(name="금요일", value=4),
+        ]
+    )
+    @app_commands.describe(date="원하는 날짜의 시간표를 볼때 사용합니다. (입력예시: 20220505)")
+    async def request_get(
+        self,
+        interaction: Interaction,
+        grade: int,
+        dddep_name: app_commands.Choice[str],
+        class_name: int = 1,
+        weekday: Optional[app_commands.Choice[int]] = None,
+        date: Optional[int] = None,
+    ) -> None:
+        cur_date = datetime.datetime.now(pytz.timezone("Asia/Seoul"))
 
-        ymd = cur_date.strftime('%Y%m%d')
+        ymd = cur_date.strftime("%Y%m%d")
         if weekday != None:
             if cur_date.weekday() == 5 or cur_date.weekday() == 6:
                 cur_date += datetime.timedelta(weeks=1)
-            cur_date -= datetime.timedelta(
-                days=cur_date.weekday() - weekday.value)
-            ymd = cur_date.strftime('%Y%m%d')
+            cur_date -= datetime.timedelta(days=cur_date.weekday() - weekday.value)
+            ymd = cur_date.strftime("%Y%m%d")
         if date != None:
-            cur_date = datetime.datetime.strptime(str(date), '%Y%m%d')
-            ymd = cur_date.strftime('%Y%m%d')
+            cur_date = datetime.datetime.strptime(str(date), "%Y%m%d")
+            ymd = cur_date.strftime("%Y%m%d")
 
         if dddep_name == None:
-            error_embed = discord.Embed(title='으엑, 에러가…', color=0xFF0000, description='학과명이 작성되어지지 않았어요!').set_footer(text=f'YMD:{ymd} / ppaka')
+            error_embed = discord.Embed(
+                title="으엑, 에러가…", color=0xFF0000, description="학과명이 작성되어지지 않았어요!"
+            ).set_footer(text=f"YMD:{ymd} / ppaka")
             await interaction.response.send_message(embed=error_embed)
             return
-        dddep = re.sub(r'[0-9]+', '', dddep_name.value)
+        dddep = re.sub(r"[0-9]+", "", dddep_name.value)
 
-        APTP_OFCDC_SC_CODE = 'B10'
-        SD_SCHUL_CODE = '7010911'
+        APTP_OFCDC_SC_CODE = "B10"
+        SD_SCHUL_CODE = "7010911"
         ALL_TI_YMD = ymd
         DDDEP_NM = dddep
         GRADE = grade
         CLASS_NM = class_name
-        req_url = f'https://open.neis.go.kr/hub/hisTimetable?KEY={NEIS_KEY}&Type=json&ATPT_OFCDC_SC_CODE={APTP_OFCDC_SC_CODE}&SD_SCHUL_CODE={SD_SCHUL_CODE}&ALL_TI_YMD={ALL_TI_YMD}&DDDEP_NM={parse.quote(DDDEP_NM)}&GRADE={GRADE}&CLASS_NM={CLASS_NM}'
+        req_url = f"https://open.neis.go.kr/hub/hisTimetable?KEY={NEIS_KEY}&Type=json&ATPT_OFCDC_SC_CODE={APTP_OFCDC_SC_CODE}&SD_SCHUL_CODE={SD_SCHUL_CODE}&ALL_TI_YMD={ALL_TI_YMD}&DDDEP_NM={parse.quote(DDDEP_NM)}&GRADE={GRADE}&CLASS_NM={CLASS_NM}"
 
         await interaction.response.defer(ephemeral=False)
 
         try:
             req = requests.get(req_url)
 
-        except requests.exceptions.Timeout as errd:
-            print("Timeout Error : ", errd)
-            await interaction.response.edit_message(content='에러 발생! [Timeout Error]')
+        except requests.exceptions.Timeout as e:
+            print("Timeout Error : ", e)
+            await interaction.response.edit_message(content="에러 발생! [Timeout Error]")
             return
 
-        except requests.exceptions.ConnectionError as errc:
-            print("Error Connecting : ", errc)
-            await interaction.response.edit_message(content='에러 발생! [Connection Error]')
+        except requests.exceptions.ConnectionError as e:
+            print("Error Connecting : ", e)
+            await interaction.response.edit_message(content="에러 발생! [Connection Error]")
             return
 
-        except requests.exceptions.HTTPError as errb:
-            print("Http Error : ", errb)
-            await interaction.response.edit_message(content='에러 발생! [Http Error]')
+        except requests.exceptions.HTTPError as e:
+            print("Http Error : ", e)
+            await interaction.response.edit_message(content="에러 발생! [Http Error]")
             return
 
-        # Any Error except upper exception
-        except requests.exceptions.RequestException as erra:
-            print("AnyException : ", erra)
-            er_msg = ''
-            if hasattr(e, 'message'):
-                er_msg = e.message
-            else:
-                er_msg = e
-            await interaction.response.edit_message(content=f'알 수 없는 오류가 발생했어요! {str(er_msg)}')
+        except requests.exceptions.RequestException as e:
+            print("AnyException : ", e)
+            er_msg = e
+            await interaction.response.edit_message(
+                content=f"알 수 없는 오류가 발생했어요! {str(er_msg)}"
+            )
             return
 
         json_data = dict(req.json())
-        if 'hisTimetable' not in json_data.keys():
-            if json_data['RESULT']['CODE'] == 'INFO-200':
-                error_embed = discord.Embed(title='으엑, 에러가…', color=0xFF0000, description='해당하는 데이터가 없습니다.').add_field(
-                    name='에러코드', value='INFO-200').set_footer(text=f'YMD:{ymd} / ppaka')
+        if "hisTimetable" not in json_data.keys():
+            if json_data["RESULT"]["CODE"] == "INFO-200":
+                error_embed = (
+                    discord.Embed(
+                        title="으엑, 에러가…", color=0xFF0000, description="해당하는 데이터가 없습니다."
+                    )
+                    .add_field(name="에러코드", value="INFO-200")
+                    .set_footer(text=f"YMD:{ymd} / ppaka")
+                )
                 await interaction.edit_original_response(embed=error_embed)
             return
 
-        if json_data['hisTimetable'][0]['head'][1]['RESULT']['CODE'] != 'INFO-000':
-            error_embed = discord.Embed(title='으엑, 에러가…', color=0xFF0000, description='나이스에서 데이터를 불러올 수 없습니다.').add_field(
-                name='에러코드', value=f"{json_data['hisTimetable'][0]['head'][1]['RESULT']['CODE']}").set_footer(text=f'YMD:{ymd} / ppaka')
+        if json_data["hisTimetable"][0]["head"][1]["RESULT"]["CODE"] != "INFO-000":
+            error_embed = (
+                discord.Embed(
+                    title="으엑, 에러가…",
+                    color=0xFF0000,
+                    description="나이스에서 데이터를 불러올 수 없습니다.",
+                )
+                .add_field(
+                    name="에러코드",
+                    value=f"{json_data['hisTimetable'][0]['head'][1]['RESULT']['CODE']}",
+                )
+                .set_footer(text=f"YMD:{ymd} / ppaka")
+            )
             await interaction.edit_original_response(embed=error_embed)
             return
 
-        if 'row' not in dict(json_data['hisTimetable'][1]).keys():
-            error_embed = discord.Embed(title='으엑, 에러가⋯', color=0xFF0000, description='시간표 데이터를 정상적으로 불러올 수 없습니다. [내부 구문 문제]').set_footer(
-                text=f'YMD:{ymd} / ppaka')
+        if "row" not in dict(json_data["hisTimetable"][1]).keys():
+            error_embed = discord.Embed(
+                title="으엑, 에러가⋯",
+                color=0xFF0000,
+                description="시간표 데이터를 정상적으로 불러올 수 없습니다. [내부 구문 문제]",
+            ).set_footer(text=f"YMD:{ymd} / ppaka")
             await interaction.edit_original_response(embed=error_embed)
             return
 
-        letters = '0123456789ABCDEF'
-        color = '0x'
-        for i in range(0, 6):
+        letters = "0123456789ABCDEF"
+        color = "0x"
+        for _ in range(0, 6):
             color += letters[random.randrange(0, 16)]
-        embed = discord.Embed(title=f'{DDDEP_NM} {GRADE}-{CLASS_NM}', color=int(
-            color, 0), description=f'{daylist[cur_date.weekday()]} {DDDEP_NM} {GRADE}학년 {CLASS_NM}반 시간표를 찾아왔어요!').set_footer(text=f'YMD:{ymd} / ppaka')
-        for e in json_data['hisTimetable'][1]['row']:
-            embed.add_field(name=f"{e['PERIO']}교시",
-                            value=f"{e['ITRT_CNTNT'].replace('* ', '').strip()}", inline=False)
+        embed = discord.Embed(
+            title=f"{DDDEP_NM} {GRADE}-{CLASS_NM}",
+            color=int(color, 0),
+            description=f"{day_list[cur_date.weekday()]} {DDDEP_NM} {GRADE}학년 {CLASS_NM}반 시간표를 찾아왔어요!",
+        ).set_footer(text=f"YMD:{ymd} / ppaka")
+        for e in json_data["hisTimetable"][1]["row"]:
+            embed.add_field(
+                name=f"{e['PERIO']}교시",
+                value=f"{e['ITRT_CNTNT'].replace('* ', '').strip()}",
+                inline=False,
+            )
         await interaction.edit_original_response(embed=embed)
 
 
 async def setup(bot: commands.Bot) -> None:
-    await bot.add_cog(
-        get_timetable_day(bot)
-    )
+    await bot.add_cog(get_timetable_day(bot))
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import os
-from discord import *
+import discord
 from discord import Intents
 from discord.ext import commands
 from discord import Game
 from discord import Status
 
-APP_ID = '818800581391941649'
-DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+APP_ID = "818800581391941649"
+DISCORD_TOKEN = os.environ["DISCORD_TOKEN"]
 
 intents = Intents.default()
 intents.message_content = True
@@ -15,14 +15,12 @@ intents.message_content = True
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(
-            command_prefix='!',
+            command_prefix="!",
             intents=Intents.all(),
             sync_command=True,
-            application_id=APP_ID
+            application_id=APP_ID,
         )
-        self.initial_extension = [
-            'Cogs.get_timetable_day'
-        ]
+        self.initial_extension = ["Cogs.get_timetable_day"]
 
     async def setup_hook(self):
         for ext in self.initial_extension:
@@ -30,11 +28,13 @@ class MyBot(commands.Bot):
         await bot.tree.sync()
 
     async def on_ready(self):
-        print('login as')
+        print("login as")
+        if type(self.user) != discord.ClientUser:
+            raise TypeError("self.user is not ClientUser Type")
         print(self.user.name)
         print(self.user.id)
-        print('===============')
-        game = Game('/시간표 를 쳐보라고 말')
+        print("===============")
+        game = Game("/시간표 를 쳐보라고 말")
         await self.change_presence(status=Status.online, activity=game)
 
 
